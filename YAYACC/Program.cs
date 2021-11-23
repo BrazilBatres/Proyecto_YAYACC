28798e7e0e4e1986fe746b3e887b979ec0b36386
0112358132134-2026 <55027767+0112358132134-2026@users.noreply.github.com>
2021-11-23 00:01:53 -0600

    YA
    FIN

--- YAYACC/Program.cs
+++ YAYACC/Program.cs
@@ -1,24 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-
 namespace YAYACC
 {
     class Program
     {
         static void Main(string[] args)
-        {            
-            //¡TODO OK!
+        {                        
             Parser parser = new Parser();
             try
             {
-                parser.Parse("Gramatica_1.y");
-                Console.WriteLine("Expresión OK");
+                parser.Parse(args[0]);
+                Console.WriteLine("Grammar OK!");
                 parser.grammar.Print();
-
                 parser.grammar.BuildParser();
-                parser.grammar.ParserGrammar("float\'");
-                Console.WriteLine("ACCEPT");
-                Console.WriteLine("Press any key to continue...");
+
+                bool _continue = true;
+                while (_continue)
+                {
+                    Console.Write("Insert a word to analize: ");
+                    string _word = Console.ReadLine();
+
+                    try
+                    {
+                        parser.grammar.ParserGrammar(_word);
+                        Console.WriteLine("ACCEPT");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+                    Console.WriteLine("If you want to continue, please press C key");
+                    string option = Console.ReadLine();
+                    if (option.ToUpper() != "C")
+                    {
+                        _continue = false;
+                    }
+                }
+                Console.WriteLine("Press any key to finish...");
                 Console.ReadKey();
             }
             catch (Exception e)
